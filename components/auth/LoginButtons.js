ffd6e8e5ec23be5864c494697aa5f4583f3bc980
Tommy Barvåg <tommy@barvaag.com>
2021-05-05 09:54:57 +0200

    Add loading animation to login page

--- components/auth/LoginButtons.js
+++ components/auth/LoginButtons.js
@@ -3,12 +3,24 @@ import * as React from "react";
 import Button from "../button";
 import useAuthProviders from "./hooks/useAuthProviders";
 
-export default function LoginButtons() {
+export default function LoginButtons({ onClick, ...other }) {
   const authProviders = useAuthProviders();
   const providers = React.useMemo(() => Object.keys(authProviders ?? {}), [authProviders]);
 
   return (
-    <Button onClick={() => (providers.length === 1 ? signIn(providers[0]) : signIn())}>
+    <Button
+      onClick={() => {
+        if (onClick) {
+          onClick();
+        }
+
+        if (providers.length === 1) {
+          signIn(providers[0]);
+        } else {
+          signIn();
+        }
+      }}
+    >
       Login
     </Button>
   );
--- pages/login.js
+++ pages/login.js
@@ -7,14 +7,43 @@ import Text from "../components/text";
 
 export default function Login({ session }) {
   const router = useRouter();
+  const [isLoading, setIsLoading] = React.useState(!!session);
 
   React.useEffect(() => {
     if (session) {
+      setIsLoading(true);
       router.push("/");
     }
   }, [session]);
 
-  return (
+  return isLoading ? (
+    <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-white dark:bg-black text-gray-900 dark:text-gray-100 opacity-75 flex flex-col items-center justify-center">
+      <div className="mb-6">
+        <svg
+          className="animate-spin -ml-1 mr-3 h-16 w-16 text-green-500 text:bg-green-400"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </div>
+      <Heading variant="pageHeading">Logging you in</Heading>
+      <Text>This may take a few seconds, please don't close this page.</Text>
+    </div>
+  ) : (
     <div className="relative bg-white dark:bg-black overflow-hidden h-screen">
       <div className="max-w-7xl mx-auto h-full">
         <div className="relative z-10 pb-8 bg-white dark:bg-black sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32 h-full">
@@ -38,7 +67,7 @@ export default function Login({ session }) {
               </Heading>
               <Text>Login to see your estimated salary.</Text>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                <LoginButtons />
+                <LoginButtons onClick={() => setIsLoading(true)} />
               </div>
             </div>
           </main>
@@ -46,7 +75,7 @@ export default function Login({ session }) {
       </div>
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full opacity-75"
           src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
           alt=""
         />
