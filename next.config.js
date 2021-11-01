22c871663aedf2bc85f688604178dcffe0e061b0
Tommy Barvåg <tommy@barvaag.com>
2021-11-01 11:14:48 +0100

    Add output file tracing false to temporarily fix cache error at vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  // https://github.com/vercel/next.js/issues/30347#issuecomment-953959581
+  // to fix build error SyntaxError: Named export 'createContext' not found.
+  // The requested module 'react' is a CommonJS module, which may not support
+  // all module.exports as named exports.
+  // TODO: Remove once fixed in future release
+  outputFileTracing: false,
   future: {
     strictPostcssConfiguration: true
   },
