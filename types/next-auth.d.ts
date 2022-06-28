e1c9e822f0a8738a88f6e33a4dd3346c58bbbd1b
Tommy Barvåg <tommy@barvaag.com>
2022-06-28 22:28:58 +0200

    Add type file for next-auth

---
+++ types/next-auth.d.ts
@@ -0,0 +1,33 @@
+import { DefaultSession } from "next-auth";
+import "next-auth/jwt";
+import { User } from ".";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: DefaultSession["user"] &
+      User & {
+        image: string;
+      };
+    expires: ISODateString;
+    accessToken: string;
+    accessTokenExpires: number;
+  }
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    name: string;
+    email: string;
+    picture: string;
+    sub: string;
+    iat: number;
+    exp: number;
+    jti: string;
+    dbUser?: User;
+  }
+}
