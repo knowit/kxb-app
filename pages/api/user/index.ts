b73cf1b97027635a21bd4eb41a6f0f1fea8526d8
Tommy Barvåg <tommy@barvaag.com>
2023-03-22 14:18:51 +0100

    Add option to allow session validation bypass with api key

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,10 +1,42 @@
-import prismaUser from "@/lib/prismaUser";
+import prisma from "@/lib/prisma";
 import { sessionUserIsAdmin } from "@/utils/sessionUtils";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 
+async function getAllUsers(options = {
+  includeWorkDayDetails: false,
+  includeFeedback: false
+}) {
+  return (await prisma.user.findMany({
+    include: {
+      workDayDetails: options.includeWorkDayDetails,
+      feedback: options.includeFeedback
+    }
+  })).map(user => ({
+    ...user,
+    accessTokenExpires: Number(user?.accessTokenExpires ?? 0),
+    commission: Number(user?.commission ?? 0),
+    tax: Number(user?.tax ?? 0),
+    workHours: Number(user?.workHours ?? 0),
+    hourlyRate: Number(user?.hourlyRate ?? 0),
+    workDayDetails: (user?.workDayDetails ?? []).map(workDayDetail => ({
+      ...workDayDetail,
+      extraHours: Number(workDayDetail?.extraHours ?? 0),
+      nonCommissionedHours: Number(workDayDetail?.nonCommissionedHours ?? 0)
+    }))
+  }))
+}
+
 export default async function Users(req: NextApiRequest, res: NextApiResponse) {
   const session = await getSession({ req });
+
+  // check search params for API key
+  if (req.query.apiKey && req.query.apiKey === process.env.API_KEY) {
+    return res.status(200).json((await getAllUsers({
+      includeWorkDayDetails: req.query.includeWorkDayDetails === "true",
+      includeFeedback: req.query.includeFeedback === "true"
+    })));
+  }
 
   if (!session) {
     return res.status(401).end();
@@ -15,7 +47,11 @@ export default async function Users(req: NextApiRequest, res: NextApiResponse) {
   }
 
   if (req.method === "GET") {
-    return res.status(200).json(await prismaUser.get());
+    return res.status(200).json((await getAllUsers({
+      includeWorkDayDetails: req.query.includeWorkDayDetails === "true",
+      includeFeedback: req.query.includeFeedback === "true"
+    })));
+
   }
 
   return res.send("Method not allowed.");
