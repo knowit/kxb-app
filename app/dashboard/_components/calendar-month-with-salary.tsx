a9b65d35946766990101054147a8018923315c5c
Tommy Barvåg <tommy@barvaag.com>
2023-03-24 15:46:01 +0100

    Calculate next and previous date to own const

--- app/dashboard/_components/calendar-month-with-salary.tsx
+++ app/dashboard/_components/calendar-month-with-salary.tsx
@@ -25,6 +25,17 @@ export default function CalendarMonthWithSalary({
   userEarnings?: UserEarningsDetails;
   userSettings?: UserSettings;
 }) {
+  const previousDate = new Date(
+    calendarMonth.monthNumber === 0 ? calendarMonth.year - 1 : calendarMonth.year,
+    calendarMonth.monthNumber === 0 ? 11 : calendarMonth.monthNumber - 1,
+    1
+  );
+  const nextDate = new Date(
+    calendarMonth.monthNumber === 11 ? calendarMonth.year + 1 : calendarMonth.year,
+    calendarMonth.monthNumber === 11 ? 0 : calendarMonth.monthNumber + 1,
+    1
+  );
+
   return (
     <div className="flex flex-col gap-12 lg:flex-row">
       <div className="order-1 flex max-w-[380px] flex-col gap-3 lg:-order-1 lg:min-w-[380px]">
@@ -80,9 +91,7 @@ export default function CalendarMonthWithSalary({
             {/* Go to previous month if month.MonthNumber is 0, go to previous year and month 11 */}
             <Link
               className={buttonVariants({ variant: "outline" })}
-              href={`/dashboard/year/${
-                calendarMonth.monthNumber === 0 ? calendarMonth.year - 1 : calendarMonth.year
-              }/month/${calendarMonth.monthNumber === 0 ? 11 : calendarMonth.monthNumber - 1}`}
+              href={`/dashboard/year/${previousDate.getFullYear()}/month/${previousDate.getMonth()}`}
             >
               <span className="sr-only">Forrige måned</span>
               <Icons.ChevronLeft />
@@ -94,9 +103,7 @@ export default function CalendarMonthWithSalary({
             {/* Go to next month if month.MonthNumber is 11, go to next year and month 0 */}
             <Link
               className={buttonVariants({ variant: "outline" })}
-              href={`/dashboard/year/${
-                calendarMonth.monthNumber === 11 ? calendarMonth.year + 1 : calendarMonth.year
-              }/month/${calendarMonth.monthNumber === 11 ? 0 : calendarMonth.monthNumber + 1}`}
+              href={`/dashboard/year/${nextDate.getFullYear()}/month/${nextDate.getMonth()}`}
             >
               <span className="sr-only">Neste måned</span>
               <Icons.ChevronRight />
---
+++ app/dashboard/salary-calculator/page.tsx
@@ -0,0 +1,15 @@
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+
+export const metadata = {
+  title: "Salary calculator"
+};
+
+export default async function DashboardPage() {
+  return (
+    <>
+      <h1>Salary calculator</h1>
+    </>
+  );
+}
