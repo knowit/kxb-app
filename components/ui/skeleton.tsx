de78bef833a7f53755a2596020c6ec9fb4bfbd79
Tommy Barvåg <tommy@barvaag.com>
2023-04-12 10:53:21 +0200

    Adjust bg for skeleton on light mode

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -5,7 +5,10 @@ interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
 function Skeleton({ className, ...other }: SkeletonProps) {
   return (
     <div
-      className={cn("h-5 w-2/5 animate-pulse rounded-md bg-neutral-800", className)}
+      className={cn(
+        "h-5 w-2/5 animate-pulse rounded-md bg-neutral-300 dark:bg-neutral-800",
+        className
+      )}
       {...other}
     />
   );
