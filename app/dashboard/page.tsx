937b2c5088785194f9546772b9a544c47774d71d
Tommy Barvåg <tommy@barvaag.com>
2024-01-12 10:17:04 +0100

    Test prod commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -38,6 +38,7 @@ export default async function DashboardPage() {
 
   return (
     <>
+      <h1>Erlend er sjef nå 👨🏻‍💼</h1>
       <Suspense fallback={<UserCalendarMonthWithSalarySkeleton month={calendarMonth} />}>
         <UserCalendarMonthWithSalary
           user={user.data}
